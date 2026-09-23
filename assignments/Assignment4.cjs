8660beb76e0d26321b6fcff054bcd5915d4a8a1a
harshitchahal1 <harshitchahal75@gmail.com>
2026-09-23 15:00:22 +0530

    Assignment4

--- assignments/Assignment4.cjs
+++ assignments/Assignment4.cjs
@@ -3,18 +3,15 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// Simple array to store tasks
 let tasks = [
     { id: 1, task: "Complete assignment", completed: false },
     { id: 2, task: "Study Node.js", completed: false }
 ];
 
-// GET - See all tasks
+
 app.get('/tasks', (req, res) => {
     res.status(200).json(tasks);
 });
-
-// POST - Add a new task
 app.post('/tasks', (req, res) => {
     const newTask = {
         id: tasks.length + 1,
@@ -27,7 +24,7 @@ app.post('/tasks', (req, res) => {
     res.status(201).json(newTask);
 });
 
-// PUT - Mark a task as completed
+
 app.put('/tasks/:id', (req, res) => {
     const id = parseInt(req.params.id);
 
@@ -42,7 +39,7 @@ app.put('/tasks/:id', (req, res) => {
     res.status(200).json(task);
 });
 
-// DELETE - Remove a task
+
 app.delete('/tasks/:id', (req, res) => {
     const id = parseInt(req.params.id);
 
@@ -60,7 +57,6 @@ app.delete('/tasks/:id', (req, res) => {
     });
 });
 
-// Start server
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
